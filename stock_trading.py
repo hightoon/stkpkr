7f71c9e978296266a42904a604f6192b77a1a912
hightoon <haitong.chen@gmail.com>
2016-03-25 10:39:16 +0800

    add yahoo finance

--- stock_trading.py
+++ stock_trading.py
@@ -10,10 +10,9 @@ import urllib2
 import xlrd
 import re
 import logging
-from datetime import datetime
-#from pychartdir import *
-#from FinanceChart import *
+from datetime import datetime, timedelta
 from HTMLParser import HTMLParser
+from yahoo_finance import Share
 
 
 class MyHTMLParser(HTMLParser):
@@ -116,6 +115,33 @@ class Stock:
   def fetch(self):
     return urllib2.urlopen(self._manipulate_url()).read()
 
+  def get_historical(self, num_of_days):
+    if self._id.startswith('6'):
+      stk_id = self._id + '.ss'
+    else:
+      stk_id = self._id + '.sz'
+    stk = Share(stk_id)
+    start = datetime.strftime(datetime.today() - timedelta(days=num_of_days+1),
+                              '%Y-%m-%d')
+    end   = datetime.strftime(datetime.today() - timedelta(days=1),
+                              '%Y-%m-%d')
+    hist = stk.get_historical(start, end)
+    high    = []
+    low     = []
+    opening = []
+    close   = []
+    vol     = []
+    date    = []
+    for d in hist:
+      if d.has_key('High'):
+        high.append(float(d['High']))
+        low.append(float(d['Low']))
+        opening.append(float(d['Open']))
+        close.append(float(d['Close']))
+        vol.append(float(d['Volume']))
+        date.append(d['Date'])
+    return high, low, opening, close, vol, date
+
 class GooGleFinance:
   def __init__(self, stock, num_of_days=50):
     self._stock = stock
@@ -137,11 +163,11 @@ class GooGleFinance:
 #    FinanceChart.__init__(self, pixel)
 
 def pick_stock(stockid):
-  high, low, opening, close, vol, date = get_google_fin_data(stockid)
-  #print date
+  stk = Stock('', stockid, None)
+  high, low, opening, close, vol, date = stk.get_historical(45)
+  print stockid
   if len(close)<4:
     print 'empty'
-    pass
   else:
     thirty_avg_vol = sum(vol[1:11])/10
     if drop_for_days(5, close[:5]):
@@ -149,7 +175,7 @@ def pick_stock(stockid):
       logging.info("%s going down for 5 days, @%s"%(stockid, date[0]))
     #elif up_for_days(3, close) and up_for_days(3, vol):
     #  print stockid, "going up"
-    if vol[0] / thirty_avg_vol > 3:
+    if thirty_avg_vol and vol[0] / thirty_avg_vol > 3:
       if close[0] > close[1] and close[0] == high[0]:
         #print stockid, 'rising with large volume, @%s'%date[0]
         logging.info('%s rising with large volume, @%s'%(stockid, date[0]))
@@ -235,113 +261,7 @@ def get_google_fin_data(stock):
   return highdata, lowdata, opendata, closedata, volume, date
 
 
-
-if __name__ != '__main__':
-  stockid = '600036'
-  periods = [Period(2014, 4), Period(2015, 1), Period(2015, 2)]
-  stk = [Stock('RongZhiLian', stockid, p) for p in periods]
-  parser = MyHTMLParser()
-  parser.feed('\n'.join([s.fetch() for s in stk]))
-  #print sorted(parser.get().keys())
-  stockdata = parser.get()
-  date = sorted(stockdata.keys())
-  highdata = []
-  lowdata = []
-  opendata = []
-  closedata = []
-  volume = []
-  for d in date:
-    highdata.append(stockdata[d][1])
-    lowdata.append(stockdata[d][3])
-    opendata.append(stockdata[d][0])
-    closedata.append(stockdata[d][2])
-    volume.append(stockdata[d][-1])
-
-  # Create a XYChart object of size 600 x 350 pixels
-  c = XYChart(1800, 900)
-
-  # Set the plotarea at (50, 25) and of size 500 x 250 pixels. Enable both the
-  # horizontal and vertical grids by setting their colors to grey (0xc0c0c0)
-  c.setPlotArea(100, 50, 1500, 600).setGridColor(0xc0c0c0, 0xc0c0c0)
-
-  # Add a title to the chart
-  #c.addTitle("Universal Stock Index")
-
-  # Add a custom text at (50, 25) (the upper left corner of the plotarea). Use 12 pts
-  # Arial Bold/blue (4040c0) as the font.
-  #c.addText(50, 25, "(c) Global XYZ ABC Company", "arialbd.ttf", 12, 0x4040c0)
-
-  # Add a title to the x axis
-  #c.xAxis().setTitle("Jan 2001")
-
-  # Set the labels on the x axis. Rotate the labels by 45 degrees.
-  xlables = ['%d-%d-%d'%d for d in date]
-  c.xAxis().setLabels(xlables).setFontAngle(90)
-
-  # Add a title to the y axis
-  #c.yAxis().setTitle("Universal Stock Index")
-
-  # Draw the y axis on the right hand side of the plot area
-  c.setYAxisOnRight(1)
-
-  # Add a CandleStick layer to the chart using green (00ff00) for up candles and red
-  # (ff0000) for down candles
-  layer = c.addCandleStickLayer(highdata, lowdata, opendata, closedata, 0xff0000,
-      0x00ff00)
-
-  # Set the line width to 2 pixels
-  layer.setLineWidth(3)
-
-  # Output the chart
-  c.makeChart("candlestick.png")
-
-  #----------------------------------------------------------------------------#
-  # Create a FinanceChart object of width 640 pixels
-  c = MyFinanceChart(1240)
-
-  # Add a title to the chart
-  c.addTitle(stockid)
-
-  # Set the data into the finance chart object
-  timestamp = [chartTime(d[0], d[1], d[2]) for d in date]
-  c.setData(timestamp, highdata, lowdata, opendata, closedata, volume, 30)
-
-  # Add the main chart with 240 pixels in height
-  c.addMainChart(500)
-
-  # Add a 5 period simple moving average to the main chart, using brown color
-  c.addSimpleMovingAvg(5, 0x663300)
-
-  # Add a 20 period simple moving average to the main chart, using purple color
-  c.addSimpleMovingAvg(20, 0x9900ff)
-
-  # Add HLOC symbols to the main chart, using green/red for up/down days
-  c.addHLOC(0xcc0000, 0x008000)
-
-  # Add 20 days bollinger band to the main chart, using light blue (9999ff) as the
-  # border and semi-transparent blue (c06666ff) as the fill color
-  c.addBollingerBand(20, 2, 0x9999ff, 0xc06666ff)
-
-  # Add a 75 pixels volume bars sub-chart to the bottom of the main chart, using
-  # green/red/grey for up/down/flat days
-  c.addVolBars(75, 0xff9999, 0x99ff99, 0x808080)
-
-  # Append a 14-days RSI indicator chart (75 pixels high) after the main chart. The
-  # main RSI line is purple (800080). Set threshold region to +/- 20 (that is, RSI = 50
-  # +/- 25). The upper/lower threshold regions will be filled with red (ff0000)/blue
-  # (0000ff).
-  c.addRSI(105, 14, 0x800080, 20, 0xff0000, 0x0000ff)
-
-  # Append a 12-days momentum indicator chart (75 pixels high) using blue (0000ff)
-  # color.
-  c.addMomentum(105, 12, 0x0000ff)
-
-  c.addMACD(105, 5, 15, 30, 0xff0000, 0x00ff00, 0x0000ff)
-
-  # Output the chart
-  c.makeChart(stockid+".png")
-
-else:
+if __name__ == '__main__':
   logging.basicConfig(filename='stocks.log', format='%(asctime)s %(message)s', level=logging.DEBUG)
   table = xlrd.open_workbook('astocks.xls').sheets()[0]
   nrows, ncols = table.nrows, table.ncols
@@ -352,7 +272,6 @@ else:
     if id != []:
       try:
         pick_stock(id[0].strip(''))
-        #get_google_fin_data(id[0].strip(''))
       except KeyboardInterrupt:
         import sys
         sys.exit()
